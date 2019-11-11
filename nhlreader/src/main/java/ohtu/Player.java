a396f80fd58bd6257adde5315a6a5b9edbc94fb1
Noora Virolainen <vino@cubbli18-gold.local>
2019-11-11 11:10:27 +0200

    tehtava 1 palautus

--- nhlreader/src/main/java/ohtu/Player.java
+++ nhlreader/src/main/java/ohtu/Player.java
@@ -1,8 +1,16 @@
-
 package ohtu;
 
+import java.util.Date;
+
 public class Player {
+
+    private int goals;
+    private int assists;
     private String name;
+    private int penalties;
+    private String team;
+    private String nationality;
+    private Date birthday;
 
     public void setName(String name) {
         this.name = name;
@@ -12,9 +20,41 @@ public class Player {
         return name;
     }
 
+    public void setTeaam(String team) {
+        this.team = team;
+    }
+
+    public String getTeam() {
+        return this.team;
+    }
+
+    public void setGoals(int goals) {
+        this.goals = goals;
+    }
+
+    public int getGoals() {
+        return goals;
+    }
+
+    public void setAssists(int assists) {
+        this.assists = assists;
+    }
+
+    public int getAssists() {
+        return this.assists;
+    }
+    
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+    
+    public String getNationality(){
+        return this.nationality;
+    }
+
     @Override
     public String toString() {
-        return name;
+        return name + " team " +team + " goals " + goals + " assists " + assists;
     }
-      
+
 }
