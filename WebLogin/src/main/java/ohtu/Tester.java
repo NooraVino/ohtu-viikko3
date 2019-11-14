75b285f687d641933b2a7e1e412054f5e0a20867
Noora Virolainen <vino@cubbli18-gold.local>
2019-11-14 09:54:06 +0200

    tehtävä8

---
+++ WebLogin/src/main/java/ohtu/Tester.java
@@ -0,0 +1,68 @@
+package ohtu;
+
+import java.util.Random;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Tester {
+
+    public static void main(String[] args) {
+        WebDriver driver = new FirefoxDriver();
+    
+        final String URL = "http://localhost:4567";
+        driver.get(URL);
+
+        // WebElement element = driver.findElement(By.linkText("login"));
+        // element.click();
+
+        // sleep(2);
+
+        // element = driver.findElement(By.name("username"));
+        // element.sendKeys("pekka");
+        // element = driver.findElement(By.name("password"));
+        // element.sendKeys("akkep2");
+        // sleep(2);
+        // element.submit();
+
+    
+        Random r = new Random();
+        sleep(2);
+        
+        WebElement element = driver.findElement(By.linkText("register new user"));
+        element.click();
+
+        sleep(2);
+
+        element = driver.findElement(By.name("username"));
+        element.sendKeys("noora"+r.nextInt(100000));
+
+        element = driver.findElement(By.name("password"));
+        element.sendKeys("akkep");
+        element = driver.findElement(By.name("passwordConfirmation"));
+        element.sendKeys("akkep");
+
+        element = driver.findElement(By.name("signup"));
+        
+        sleep(2);
+        element.submit();
+
+        sleep(2);
+        element = driver.findElement(By.linkText("continue to application mainpage"));
+        element.click();
+
+        sleep(2);
+        element = driver.findElement(By.linkText("logout"));
+        element.click();
+        sleep(3);
+        driver.quit();
+    }
+    
+    private static void sleep(int n){
+        try{
+            Thread.sleep(n*1000);
+        } catch(Exception e){}
+    }
+}
